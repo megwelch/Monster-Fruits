5b5ad4abb57c32aae14435204c518291846646a2
megwelch <meghanmwelch@gmail.com>
2022-10-04 09:59:40 -0400

    routes added

---
+++ models/fruit.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////
+// Our Schema and Model for the Fruit Resource
+/////////////////////////////////////////////////
+const mongoose = require("mongoose") // import mongoose
+
+// we;re going to pull the Schema and model from mongoose
+// we'll use a syntax called 'destructuring'
+const { Schema, model } = mongoose
+
+// fruits schema
+const fruitSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+// make the fruit model
+// the model method takes two args
+// the first is what we will call our model
+// the second is what we will use to build our model
+const Fruit = model('Fruit', fruitSchema)
+
+/////////////////////////////////////////////////
+// Export our Model
+/////////////////////////////////////////////////
+module.exports = Fruit
--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const express = require("express") // import express
 const morgan = require("morgan") // import morgan
 const mongoose = require("mongoose") // import mongoose
 const path = require("path") // import path module
-const { on } = require("events")
+// const { on } = require("events")
+
+/////////////////////////////////////////////
+// Import Our Models
+/////////////////////////////////////////////
+const Fruit = require('./models/fruit')
 
 /////////////////////////////////////////////
 // Databse Connection
@@ -41,7 +46,7 @@ mongoose.connection
 // middleware runs before all the routes, every request to process through our middleware before mongoose does anything with it
 
 app.use(morgan('tiny')) 
-// ^^^ this is for request logging, the'tiny argument declard what size of morgan log to use
+// ^^^ this is for request logging, the 'tiny' argument declares what size of morgan log to use
 app.use(express.urlencoded({ extended:true }))
 // ^^^ request bodies(useful for POST and PUT requests)
 app.use(express.static('public'))
@@ -52,18 +57,119 @@ app.use(express.json())
 /////////////////////////////////////////////
 // Routes
 /////////////////////////////////////////////
-app.get('/', (req, res) => {
-    res.send('your server is running, better go out and catch it')
+app.get("/", (req, res) => {
+    res.send("Your server is running, better go out and catch it")
+    // you can also send html as a string from res.send
+    // res.send("<small style='color: red'>Your server is running, better go out and catch it</small>")
 })
 
-// app.get('/donut', (req, res) => {
-//     res.send('welcome to the donut page')
-// })
+// Here, we're going to set up a seed route
+// this will seed our database for us, so we have some starting resources
+// there are two ways we're going to talk about seeding a db
+// routes -> they work, but they're not best practices
+// seed scripts -> they work, and they are best practices
+app.get("/fruits/seed", (req, res) => {
+    // array of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false },
+    ]
+
+    // Delete every fruit in the db
+    Fruit.deleteMany({})
+        .then(() => {
+            // seed with the starter fruits array
+            Fruit.create(startFruits)
+                .then(data => {
+                    res.json(data)
+                })
+        })
+})
+
+// GET request
+// index route -> shows all instances of a document in the db
+app.get("/fruits", (req, res) => {
+    // in our index route, we want to use mongoose model methods to get our data
+    Fruit.find({})
+        .then(fruits => {
+            // this is fine for initial testing
+            // res.send(fruits)
+            // this the preferred method for APIs
+            res.json({ fruits: fruits })
+        })
+        .catch(err => console.log(err))
+})
+
+// POST request
+// create route -> gives the ability to create new fruits
+app.post('/fruits', (req, res) => {
+    // here, we'll get something called a request body
+    // inside this function, that will be referred to as req.body
+    // we'll use the mongoose method 'create' to make a new fruit
+    Fruit.create(req.body)
+        .then(fruit => {
+            // send the user a '201 created' response, along with the new fruit
+            res.status(201).json({ fruit: fruit.toObject() })
+        })
+        .catch(error => console.log(error))
+})
+
+// PUT request
+// update route -> updates a specfic fruit
+app.put('/fruits/:id', (req, res) => {
+    // console.log(req.params)
+    // res.send(`nothing yet, but we're getting there`)
+    const id = req.params.id
+    // for now, we'll use a simple mongoose model, eventually we'll update this(and all) routes and we'll use a different method
+    // we're using findByIdAndUpdate, which needs three arguments
+    // it needs an id, it needs the req.body, and whether the info is new
+    Fruit.findByIdAndUpdate(id, req.body, {new: true})
+        .then(fruit => {
+            console.log('the fruit from update', fruit)
+            // update success is called '204 - no content'
+            res.sendStatus(204)
+        })
+        .catch(err => console.log(err))
+})
+
+// DELETE request
+// destroy route -> finds and deletes a single resource (fruit)
+app.delete("/fruits/:id", (req, res) => {
+    // grab id from the request
+    const id = req.params.id
+    // find and delete the fruit
+    Fruit.findByIdAndRemove(id)
+        .then(fruit => {
+            res.sendStatus(204)
+        })
+        .catch(err => res.json(err))
+    // send a 204 if successful
+    // send the error if not
+})
+
+// SHOW request
+// read route -> finds and displays a single resource
+app.get("/fruits/:id", (req, res) => {
+    // console.log("this is the request", req)
+    // in our index route, we want to use mongoose model methods to get our data
+    const id = req.params.id
+    Fruit.findById(id)
+        .then(fruit => {
+            // this is fine for initial testing
+            // res.send(fruits)
+            // this the preferred method for APIs
+            res.json({ fruit: fruit })
+        })
+        .catch(err => console.log(err))
+})
 
 /////////////////////////////////////////////
 // Server Listener
 /////////////////////////////////////////////
 const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`not listening to the sweet sounds of port: ${PORT}`))
+app.listen(PORT, () => console.log(`now listening to the sweet sounds of port: ${PORT}`))
 
 // END
